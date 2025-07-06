1e629263fb65e29a25196f9c18232efbf44bea9e
LostInTheBugs <frederic.lassalle@free.fr>
2025-07-06 10:14:52 +0200

    main

--- public/get-markets.py
+++ public/get-markets.py
@@ -6,7 +6,7 @@ list_allmarkets = Public()
 def get_markets():
     market = list_allmarkets.get_markets()
     for market in market:
-        print(f"{market['symbol']})
+        print(f"{market['symbol']}")
 
 if __name__ == "__main__":
     get_markets()
