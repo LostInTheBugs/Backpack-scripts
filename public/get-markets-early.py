36cbd039c7f3a0f6e845369ca8a5a6d9422e27c2
LostInTheBugs <frederic.lassalle@free.fr>
2025-07-06 10:54:22 +0200

    main

---
+++ public/get-markets-early.py
@@ -0,0 +1,23 @@
+from bpx.public import Public
+from tabulate import tabulate
+
+list_allmarkets = Public()
+
+def get_markets():
+    headers = ["Symbol", "Base Symbol", "Quote Symbol", "Market Type", "Order Book State", "Created At"]
+    table = []
+    market = list_allmarkets.get_markets()
+    for market in market:
+        table.append([
+            market['symbol'],
+            market['baseSymbol'],
+            market['quoteSymbol'],
+            market['marketType'],
+            market['orderBookState'],
+            market['createdAt'],     
+        ])
+
+    print(tabulate(table, headers=headers, tablefmt="grid"))
+
+if __name__ == "__main__":
+    get_markets()
