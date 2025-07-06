9e548cf0b52079511d3694981ada5c94a86357a7
LostInTheBugs <frederic.lassalle@free.fr>
2025-07-06 10:20:54 +0200

    main

--- public/get-markets.py
+++ public/get-markets.py
@@ -4,12 +4,24 @@ from tabulate import tabulate
 list_allmarkets = Public()
 
 def get_markets():
-    headers = ["Symbol"]
+    headers = ["Symbol", "Base Symbol", "Quote Symbol", "Market Type", "Filters", "IMF Function", "MMF Function", "Funding Interval", "Funding Rate Upper Bound", "Funding Rate Lower Bound", "Open Interest Limit", "Order Book State", "Created At"]
     table = []
     market = list_allmarkets.get_markets()
     for market in market:
         table.append([
             market['symbol'],
+            market['baseSymbol'],
+            market['quoteSymbol'],
+            market['marketType'],
+            market['filters'],
+            market['imfFunction'],
+            market['mmfFunction'],
+            market['fundingInterval'],
+            market['fundingRateUpperBound'],
+            market['fundingRateLowerBound'],
+            market['openInterestLimit'],
+            market['orderBookState'],
+            market['createdAt'],            
         ])
 
     print(tabulate(table, headers=headers, tablefmt="grid"))
