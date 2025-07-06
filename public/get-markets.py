327945f7ec631f96632fa16d377fa41cc353db0e
LostInTheBugs <frederic.lassalle@free.fr>
2025-07-06 10:10:42 +0200

    main

--- public/get-markets.py
+++ public/get-markets.py
@@ -1,11 +1,12 @@
-from bpx import API
+from bpx.public import Public
 
-api = API()
+list_allmarkets = Public()
 
 def get_markets():
-    markets = api.get_markets()
+    markets = list_allmarkets.get_markets()
     for market in markets:
         print(f"{market['name']:15} | {market['status']:8} | base: {market['baseAssetSymbol']}, quote: {market['quoteAssetSymbol']}")
-        
+
 if __name__ == "__main__":
     get_markets()
+:q!
