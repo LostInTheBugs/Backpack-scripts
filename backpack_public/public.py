5b0a69445ee8e42f4abe42f091f0d0f83a895ea4
LostInTheBugs <frederic.lassalle@free.fr>
2025-07-22 22:06:18 +0200

    main

--- backpack_public/public.py
+++ backpack_public/public.py
@@ -7,10 +7,10 @@ def get_ohlcv(symbol: str, interval: str = "1m", limit: int = 20):
         "interval": interval,
         "limit": limit
     }
-    try:
-        resp = requests.get(url, params=params)
-        resp.raise_for_status()
-        return resp.json()
-    except requests.exceptions.HTTPError as e:
-        print(f"Erreur HTTP {resp.status_code} pour l'URL {resp.url}")
-        raise e
+    resp = requests.get(url, params=params)
+    resp.raise_for_status()
+    return resp.json()
+
+if __name__ == "__main__":
+    data = get_ohlcv("BTC_USDC_PERP")
+    print(data[:2])
