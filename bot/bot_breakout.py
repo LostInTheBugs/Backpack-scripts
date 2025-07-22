58824d6a48aef13924a2fbb2175cd3a125607723
LostInTheBugs <frederic.lassalle@free.fr>
2025-07-22 21:46:04 +0200

    main

--- bot/bot_breakout.py
+++ bot/bot_breakout.py
@@ -10,7 +10,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from read.opened_positions import get_open_positions
 from execute.open_position_usdc import open_position
 from execute.close_position_percent import close_position_percent
-from public.public import get_ohlcv  # ← tu dois avoir cette fonction
+from public import get_ohlcv
 
 # 🔑 Clés API via variables d'environnement
 public_key = os.environ.get("bpx_bot_public_key")
