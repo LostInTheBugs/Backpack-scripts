02ea0f6f3a2f2ffbd84f55bbf2daa64e8a41d55d
LostInTheBugs <frederic.lassalle@free.fr>
2025-07-27 11:42:51 +0200

    main

--- V2/main.py
+++ V2/main.py
@@ -12,7 +12,7 @@ import pytz
 from ScriptDatabase.pgsql_ohlcv import get_ohlcv_1s_sync, fetch_ohlcv_1s
 from signals.macd_rsi_breakout import get_combined_signal
 from utils.logger import log
-from utils.position_utils import position_already_open
+from utils.position_utils import position_already_open, get_open_positions
 from utils.ohlcv_utils import get_ohlcv_df
 from utils.get_market import get_market
 from utils.public import get_ohlcv, format_table_name, check_table_and_fresh_data, get_last_timestamp, load_symbols_from_file
