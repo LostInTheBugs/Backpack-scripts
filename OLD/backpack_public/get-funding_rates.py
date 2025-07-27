855a60b1d85a84b5f28c79d15a26a3cab28c3fdb
LostInTheBugs <frederic.lassalle@free.fr>
2025-07-27 12:05:20 +0200

    main

--- OLD/backpack_public/get-funding_rates.py
+++
@@ -1,6 +0,0 @@
-from bpx.public import Public
-
-public = Public()
-rates = public.get_funding_interval_rates(symbol="SOL_USDC_PERP")
-
-print(rates)
--- OLD/backpack_public/get_market.py
+++
@@ -1,9 +0,0 @@
-# public/get_market.py
-
-import requests
-
-def get_market(symbol: str):
-    url = f"https://api.backpack.exchange/v1/market/{symbol}"
-    response = requests.get(url)
-    response.raise_for_status()
-    return response.json()
