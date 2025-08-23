cda847005e9de3db77adcd4d731e940a03f7da84
LostInTheBugs <frederic.lassalle@gmail.com>
2025-08-23 12:00:08 +0200

    v0.3: merge & clean

--- utils/precision.py
+++
@@ -1,9 +0,0 @@
-from decimal import Decimal, ROUND_DOWN
-
-def adjust_to_step(value: float, step: float) -> float:
-    """
-    Arrondit une valeur vers le bas en respectant l'incrément (step).
-    """
-    if step == 0:
-        return value
-    return float(Decimal(value).quantize(Decimal(str(step)), rounding=ROUND_DOWN))
